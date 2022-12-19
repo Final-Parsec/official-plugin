70b8b79b0d2fb53d0aa09f77ab284bbe4f8d9803
Matt <Matthewbauer21@gmail.com>
2022-12-19 12:58:54 -0600

    Detect unsupported use cases, Link to blog article instructions

--- UploaderWindow.cs
+++ UploaderWindow.cs
@@ -32,8 +32,16 @@ public class UploaderWindow : EditorWindow
     [MenuItem("Final Parsec/Upload Game")]
     public static void CreateUploader()
     {
+        #if UNITY_2020_3_OR_NEWER
+        
         PlayerSettings.WebGL.decompressionFallback = true;
         EditorWindow.GetWindow<UploaderWindow>(false, "Final Parsec");
+        
+        #else
+
+        Debug.Log("Final Parsec Uploader requires 2020.3 or newer. Please upgrade to use this asset.");
+
+        #endif
     }
 
     public void BuildForWeb()
@@ -85,6 +93,8 @@ public class UploaderWindow : EditorWindow
 
     public void Upload(List<string> filesToUpload)
     {
+        #if UNITY_2020_3_OR_NEWER
+
         var formData = new List<IMultipartFormSection>();
         formData.Add(new MultipartFormDataSection("gameName", gameName));
         foreach (var fileToUpload in filesToUpload)
@@ -115,6 +125,8 @@ public class UploaderWindow : EditorWindow
             Debug.Log("Upload result: " + request.result);
             Debug.Log("Upload error: " + request.error);
         }
+
+        #endif
     }
 
     public void OnGUI()
@@ -192,7 +204,20 @@ public class UploaderWindow : EditorWindow
 
         if (!string.IsNullOrEmpty(UploaderWindow.GAME_ID))
         {
-            EditorGUILayout.SelectableLabel("Play your game at: https://www.finalparsec.com/Game/AnonymousPlay/" + UploaderWindow.GAME_ID);
+            var anonPlayUrl = $"https://www.finalparsec.com/Game/AnonymousPlay/{UploaderWindow.GAME_ID}";
+            EditorGUILayout.SelectableLabel($"Play your game at: {anonPlayUrl}");
+            if (GUILayout.Button("Play your game now"))
+            {
+                Application.OpenURL(anonPlayUrl);
+            }
+        }
+
+        GUILayout.Label("-------", EditorStyles.boldLabel);
+        var docsUrl = "https://www.finalparsec.com/Blog/ViewPost/final-parsec-upload";
+        EditorGUILayout.SelectableLabel($"Read the docs at: {docsUrl}");
+        if (GUILayout.Button("Uploader Documentation"))
+        {
+            Application.OpenURL(docsUrl);
         }
 
         EditorGUILayout.EndScrollView();
